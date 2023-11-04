34ed8019351413607d9b37f5f6f6cb8efab03f67
SewoongPark <lympsw12@naver.com>
2023-11-04 23:33:35 +0900

    Add files via upload

---
+++ src/main/java/com/example/wncsbackend/WncsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wncsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WncsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WncsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wncsbackend/WncsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wncsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WncsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
